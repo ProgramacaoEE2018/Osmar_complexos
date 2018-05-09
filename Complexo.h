ce998ad9341e335deeca69b449662af3b7dfcd0d
Osmar <osmar.ff.1996@gmail.com>
2018-05-09 18:37:44 -0300

    nova modificacao

--- Complexo.h
+++ Complexo.h
@@ -7,28 +7,12 @@ public :
 	Complexo(float xs, float ys) {
 		real = xs;
 		im = ys;
-	}
+	};
 
-	float Modulo() {
-		return sqrt(real*real + im * im);
-	}
-	Complexo Conjugado() {
-		return Complexo(real, -im);
-	}
-
-	Complexo operator+(Complexo z1) {
-		return Complexo(real + z1.real, im + z1.im);
-	}
-
-	Complexo operator-(Complexo z1) {
-		return Complexo(real - z1.real, im - z1.im);
-	}
-
-	Complexo operator*(Complexo z1) {
-		return Complexo(real * z1.real - im* z1.im, real * z1.real + im * z1.im);
-	}
-
-	Complexo operator/(Complexo z1) {
-		return Complexo((real * z1.real + im * z1.im)/(sqrt(z1.real*z1.real + z1.im * z1.im)*sqrt(z1.real*z1.real + z1.im * z1.im)), (im * z1.real - real * z1.im) / (sqrt(z1.real*z1.real + z1.im * z1.im)*sqrt(z1.real*z1.real + z1.im * z1.im)));
-	}
+	float Modulo();
+	Complexo Conjugado();
+	Complexo operator+(Complexo);
+	Complexo operator-(Complexo);
+	Complexo operator*(Complexo);
+	Complexo operator/(Complexo);
 };
